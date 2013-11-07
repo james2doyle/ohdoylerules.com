995030a51924440db964d15740b75ac04113e26d
james2doyle <james2doyle@gmail.com>
2013-11-07 09:23:46 -0500

    plugin update with new event

--- plugins/phileTwigFilters/plugin.php
+++ plugins/phileTwigFilters/plugin.php
@@ -7,23 +7,23 @@
 
 class PhileTwigFilters extends \Phile\Plugin\AbstractPlugin implements \Phile\EventObserverInterface {
 	public function __construct() {
-		\Phile\Event::registerEvent('before_render', $this);
+		\Phile\Event::registerEvent('template_engine_registered', $this);
 	}
 
 	public function on($eventKey, $data = null) {
-		if ($eventKey == 'before_render') {
+		if ($eventKey == 'template_engine_registered') {
 			// grab the first paragraph and remove all the html code
 			$excerpt = new Twig_SimpleFilter('excerpt', function ($string){
 				return strip_tags(substr($string, 0, strpos($string, "</p>") + 4));
 			});
-			$data['twig']->addFilter($excerpt);
+			$data['engine']->addFilter($excerpt);
 			// limit words function -- very rough limit due to HTML input
 			$limit_words = new Twig_SimpleFilter('limit_words', function ($string) {
 				$words = str_word_count($string, 2);
 				$pos = array_keys($words);
 				return trim(substr($string, 0, $pos[$this->settings['limit']])) . '...';
 			});
-			$data['twig']->addFilter($limit_words);
+			$data['engine']->addFilter($limit_words);
 		}
 	}
 }
