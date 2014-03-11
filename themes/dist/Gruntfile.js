c1a6289de9bfae30a738008abb95602e4a48bb7b
james2doyle <james2doyle@gmail.com>
2014-03-11 15:44:13 -0400

    update gruntfile and package.json for new build process

--- themes/dist/Gruntfile.js
+++ themes/dist/Gruntfile.js
@@ -2,16 +2,24 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>)\n' +
+        '* Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+    },
     concat: {
       options: {
-        separator: '\n'
+        separator: '\n',
+        stripBanner: true
       },
       scripts: {
         src: ['src/scripts/*.js'],
         dest: 'js/main.js'
       },
       styles: {
-        src: ['css/*.css', '!css/main.css'],
+        src: ['css/*.css', '!css/main.css', '!css/main.min.css'],
         dest: 'css/main.css'
       }
     },
@@ -47,13 +55,36 @@ module.exports = function(grunt) {
         files: ['src/scripts/*.js'],
         tasks: ['concat:scripts']
       }
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      build: {
+        files: {
+          'js/main.min.js': ['js/main.js']
+        }
+      }
+    },
+    cssmin: {
+      compress: {
+        options: {
+          banner: '<%= meta.banner %>'
+        },
+        files: {
+          'css/main.min.css': ['css/main.css']
+        }
+      }
     }
   });
 
-grunt.loadNpmTasks('grunt-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-grunt.registerTask('default', ['sass', 'concat', 'autoprefixer']);
+  grunt.registerTask('default', ['sass', 'concat', 'autoprefixer']);
+  grunt.registerTask('compress', ['sass', 'concat', 'uglify', 'autoprefixer', 'cssmin']);
 };
