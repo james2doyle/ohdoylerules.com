6e505c5a7e7627c432f1ea36a1f3ffe4a100c11e
james2doyle <james2doyle@gmail.com>
2016-08-28 15:10:06 -0700

    fix grammar in bio

--- config.php
+++ config.php
@@ -25,7 +25,7 @@ $config['github'] = 'james2doyle';
 $config['googleplus'] = '109231487156400680487';
 $config['referral_code'] = "https://www.digitalocean.com/?refcode=baa9cb97566c";
 
-$config['author_blurb'] = 'I a full-stack developer at <a href="http://invokemedia.com" target="_blank" title="Invoke">Invoke</a>, co-creator of <a href="https://github.com/PhileCMS/Phile" title="PhileCMS" target="_blank">PhileCMS</a>. I am a huge Open Source advocate and contributor to a lot of projects in my community. When I am not sitting at a computer, I am trying to perfect some other skill.';
+$config['author_blurb'] = 'I am a full-stack developer at <a href="http://invokemedia.com" target="_blank" title="Invoke">Invoke</a>, co-creator of <a href="https://github.com/PhileCMS/Phile" title="PhileCMS" target="_blank">PhileCMS</a>. I am a huge Open Source advocate and contributor to a lot of projects in my community. When I am not sitting at a computer, I am trying to perfect some other skill.';
 
 $config['author_helpline_title'] = 'Need Help?';
 $config['author_helpline'] = 'Are you having trouble with something? I can give you some advice, instruction, or <em>even look at your code</em>! All of this, for only <a href="http://bitcoin.com/" target="_blank" title="What is BitCoin?"><strong>0.02btc/hour</strong></a>. Or we can work something out.';
