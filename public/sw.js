ccd950c9c4766b9141299cee785d44626137a7fa
James Doyle <james2doyle@gmail.com>
2020-08-15 18:56:00 -0700

    Added: public folder for gh-pages

---
+++ public/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'PWA-v1';
+const expectedCaches = [CACHE_NAME];
+
+// the list of files that need to be cached
+const staticFiles = [
+  './',
+  './css/main.min.css',
+  './css/tomorrow-night.css',
+  './js/main.min.js',
+  './js/highlight.custom.js',
+  './icons/logo.svg',
+  './manifest.json',
+  './favicon.ico',
+];
+
+/**
+ * Performs install steps.
+ */
+addEventListener('install', (event) => {
+  // install this service worker as soon as a new one is available
+  skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(staticFiles)));
+});
+
+/**
+ * Handles requests: responds with cache or else network.
+ */
+addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+/**
+ * Cleans up static cache and activates the Service Worker.
+ */
+addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map((key) => {
+    if (!expectedCaches.includes(key)) {
+      return caches.delete(key);
+    }
+  }))).then(() => {
+    console.log(`${CACHE_NAME} now ready to handle fetches!`);
+    return clients.claim();
+  }));
+});
